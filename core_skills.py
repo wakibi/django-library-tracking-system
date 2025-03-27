c769f229f570df5b26694e56ef2c2405ad175ed5
Wakibi Chris <wakibic@gmail.com>
2025-03-27 23:55:39 +0300

    Python core skills

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,13 @@
 import random
-# rand_list =
 
-# list_comprehension_below_10 =
+# Create a list of 10 random numbers between 1 and 20.
+# Filter Numbers Below 10 (List Comprehension)
+# Filter Numbers Below 10 (Using filter)
 
-# list_comprehension_below_10 =
+rand_list = random.randint(1, 20) # Assumption - Only integers and 1 and 20 are inclusive
+
+# Using list comprehension
+list_comprehension_below_10 = [ i for i in rand_list if i < 10]
+
+# Using filter
+list_comprehension_below_10 = filter(lambda i: i < 10, rand_list)
