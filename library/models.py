4d62e6f02577e8c4f39a94797f7aa95acfd3e4a2
Wakibi Chris <wakibic@gmail.com>
2025-03-27 23:57:17 +0300

    Add due_date field

--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -41,6 +42,12 @@ class Loan(models.Model):
     loan_date = models.DateField(auto_now_add=True)
     return_date = models.DateField(null=True, blank=True)
     is_returned = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.book.title} loaned to {self.member.user.username}"
+    
+    def save(self, *args, **kwargs):
+        if self.loan_date:
+            self.due_date = self.loan_date + timedelta(days=20)
+        super().save(*args, **kwargs)
--- library/tasks.py
+++ library/tasks.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from celery import shared_task
 from .models import Loan
 from django.core.mail import send_mail
@@ -18,3 +19,22 @@ def send_loan_notification(loan_id):
         )
     except Loan.DoesNotExist:
         pass
+
+
+@shared_task
+def check_overdue_loans():
+    try:
+        loans = Loan.objects.select_related('book').select_related('member').filter(is_returned=False, due_date__lt= datetime.today())
+        for loan in loans:
+            member_email = loan.member.user.email
+            book_title = loan.book.title
+            send_mail(
+                subject='Loaned Book Over-due',
+                message=f'Hello {loan.member.user.username},\n\nThe return date for the book "{book_title}" you borrowed is over-due.\nPlease return it immediately!',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[member_email],
+                fail_silently=False,
+            )
+    except Loan.DoesNotExist:
+        pass
+        
